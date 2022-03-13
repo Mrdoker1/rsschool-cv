866ea0a5c29828759033057c57890605a810e293
viachaskul <fixrapdoK@gmail.com>
2022-03-14 00:54:49 +0300

    feat: add hide works button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,15 @@
-var element = document.querySelector("#menu-checkbox");
+let element = document.querySelector("#menu-checkbox");
 
+let buttons = [
+    document.querySelector("#show-more-works")
+];
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        showToggle(button.parentElement.firstElementChild.textContent, button);
+    })
+})
 
 function MenuHandler() {
     if (element.checked) {
@@ -18,4 +28,23 @@ function disableScrolling() {
 function enableScrolling() {
     document.body.style.overflow = "visible";
     document.getElementById("menu-checkbox").checked = false;
+}
+
+// showToggle
+function showToggle(type, button) {
+
+    if (type === 'Works') {
+
+        element = document.getElementById('Works').children[1];
+
+        if (element.classList.contains('hide-content')) {
+            element.classList.remove("hide-content");
+            element.classList.add("show-content");
+            button.textContent = 'Hide';
+        } else {
+            element.classList.remove("show-content");
+            element.classList.add("hide-content");
+            button.textContent = 'Show more';
+        }
+    }
 }
